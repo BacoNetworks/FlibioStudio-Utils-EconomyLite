190618ffeba1790f1cc512b23ef35e84e6e2fb88
Flibio <flibio.business@gmail.com>
2016-08-06 20:07:17 -0600

    Cache files for improved performance

---
+++ src/main/java/io/github/flibio/utils/file/InvalidPluginException.java
@@ -0,0 +1,11 @@
+package io.github.flibio.utils.file;
+
+public class InvalidPluginException extends RuntimeException {
+
+    private static final long serialVersionUID = -8576048066064901029L;
+
+    public InvalidPluginException(String message) {
+        super(message);
+    }
+
+}
